f29b99224e348a53a75e4d7d4eff7e5a9f41b31c
Chris Holcombe <xfactor973@gmail.com>
2017-12-28 10:11:01 -0800

    Make the repro even smaller

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
 extern crate gfapi_sys;
-#[macro_use]
-extern crate log;
-extern crate simplelog;
-
 use std::thread;
 
 use gfapi_sys::gluster::*;
-use simplelog::{Config, SimpleLogger};
 
 fn main() {
-    let _ = SimpleLogger::init(log::LogLevelFilter::Trace, Config::default()).unwrap();
     let mut handles: Vec<thread::JoinHandle<_>> = Vec::new();
     for _ in 0..10 {
         let handle = thread::spawn(move || {
             let cluster = match Gluster::connect("test", "localhost", 24007) {
                 Ok(c) => c,
                 Err(e) => {
-                    error!("{:?}", e);
+                    println!("{:?}", e);
                     return;
                 }
             };
